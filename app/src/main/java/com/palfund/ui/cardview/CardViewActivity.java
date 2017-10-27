b002cf3c6b1eb77480c570dda6dd68659369bdf7
cclvcs <754572407@qq.com>
2017-10-27 17:51:22 +0800

    2017/10/27

--- app/src/main/java/com/palfund/ui/cardview/CardViewActivity.java
+++ app/src/main/java/com/palfund/ui/cardview/CardViewActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.palfund.ui.R;
 
+import java.util.Random;
+
 public class CardViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+        int i = new Random().nextInt(10);
     }
 }
